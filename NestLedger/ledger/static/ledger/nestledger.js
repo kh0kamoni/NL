7a74c3d41f347064da4a37c07a2de6e081fede5d
khokamoni <khokamoni@icloud.com>
2025-02-11 04:22:29 -0500

    Remove all users add nav fix

--- NestLedger/ledger/static/ledger/nestledger.js
+++ NestLedger/ledger/static/ledger/nestledger.js
@@ -72,5 +72,18 @@
 
 
 
+    function toggleMenu() {
+        const mobileMenu = document.getElementById('mobile-menu');
+        if (mobileMenu.classList.contains('hidden')) {
+            mobileMenu.classList.remove('hidden');
+        } else {
+            mobileMenu.classList.add('hidden');
+        }
+    }
+
+
+
+
+
 
 
